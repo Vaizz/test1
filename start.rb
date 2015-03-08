3eec62a70c1270bb3545550e45eca22f5fb3eef4
Vaizz <akiba788@mail.ru>
2015-03-08 22:28:07 +0300

    update length and add weigth

--- start.rb
+++ start.rb
@@ -2,15 +2,19 @@
 public
 
   def meters
-    MyLength.new(self, :meter)
+    Length.new(self, :meter)
   end
 
   def miles
-    MyLength.new(self, :miles)
+    Length.new(self, :miles)
+  end
+
+  def pounds
+    Weight.new(self, :pounds)
   end
 
 
-class MyLength
+class Length
 
   def initialize(num, type)
     @num = num
@@ -18,10 +22,47 @@ class MyLength
   end
 
   def in(val)
-    if @type == :miles
-      @num * 0.000621371192
-    else
-      puts 'TypeMismatch'
+    case @type
+      when :meter
+        if val == :miles
+          @num * 0.000621371192
+        elsif val == :inches
+          @num * 39.3700787
+        elsif val == :centimeters
+          @num * 39.3700787
+        else
+          puts 'TypeMismatch'
+        end
+      when :miles
+        if val == :meters
+          @num * 1852
+        elsif val == :inches
+          @num * 63356.935951
+        elsif val == :centimeters
+          @num * 185200
+        else
+          puts 'TypeMismatch'
+        end
+    end
+
+  end
+
+end
+
+class Weight < Length
+
+  def in(val)
+    case @type
+      when :pounds
+        if val == :gramms
+          @num * 453.59237
+        elsif val == :kilograms
+          @num * 0.45359237
+        elsif val == :ounces
+          @num * 16.000002116438
+        else
+          puts 'TypeMismatch'
+        end
     end
   end
 
